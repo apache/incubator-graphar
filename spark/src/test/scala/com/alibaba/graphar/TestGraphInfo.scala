ce1ef3ad0edd96d5dc5c9393025b2068157315a1
acezen <qiaozi.zwb@alibaba-inc.com>
2022-12-15 10:21:38 +0800

    Update test for vertex info

--- spark/src/test/scala/com/alibaba/graphar/TestGraphInfo.scala
+++ spark/src/test/scala/com/alibaba/graphar/TestGraphInfo.scala
@@ -14,9 +14,16 @@ class GraphInfoSuite extends AnyFunSuite {
     val graph_info = yaml.load(input).asInstanceOf[GraphInfo]
 
     assert(graph_info.getName == "ldbc_sample")
-    assert(graph_info.getPrefix == "" )
+    assert(graph_info.getPrefix == "./" )
     assert(graph_info.getVertices.size() == 1)
+    val vertices = new java.util.ArrayList[String]
+    vertices.add("person.vertex.yml")
+    assert(graph_info.getVertices == vertices)
     assert(graph_info.getEdges.size() == 1)
+    val edges = new java.util.ArrayList[String]
+    edges.add("person_knows_person.edge.yml")
+    assert(graph_info.getEdges == edges)
+    assert(graph_info.getVersion == "gar/v1")
   }
 
   test("load vertex info") {
@@ -29,6 +36,40 @@ class GraphInfoSuite extends AnyFunSuite {
     assert(vertex_info.getVerticesNumFilePath() == "vertex/person/vertex_count")
     assert(vertex_info.getPrimaryKey() == "id")
     assert(vertex_info.getProperty_groups.size() == 2)
+    assert(vertex_info.getVersion == "gar/v1")
+    assert(vertex_info.getChunk_size == 100)
+
+    for ( i <- 0 to (vertex_info.getProperty_groups.size - 1) ) {
+      val property_group = vertex_info.getProperty_groups.get(i)
+      assert(vertex_info.containPropertyGroup(property_group))
+    }
+    assert(vertex_info.containProperty("id"))
+    val property_group = vertex_info.getPropertyGroup("id")
+    assert(property_group.getProperties.size == 1)
+    assert(property_group.getFile_type == "csv")
+    assert(property_group.getFile_type_in_gar == FileType.CSV)
+    assert(vertex_info.containPropertyGroup(property_group))
+    assert(vertex_info.getPropertyType("id") == GarType.INT64)
+    assert(vertex_info.isPrimaryKey("id"))
+    assert(vertex_info.getFilePath(property_group, 0) == "vertex/person/id/part0/chunk0")
+    assert(vertex_info.getFilePath(property_group, 4) == "vertex/person/id/part4/chunk0")
+
+    assert(vertex_info.containProperty("firstName"))
+    val property_group_2 = vertex_info.getPropertyGroup("firstName")
+    assert(property_group_2.getProperties.size == 3)
+    assert(property_group_2.getFile_type == "csv")
+    assert(property_group_2.getFile_type_in_gar == FileType.CSV)
+    assert(vertex_info.containPropertyGroup(property_group_2))
+    assert(vertex_info.getPropertyType("firstName") == GarType.STRING)
+    assert(vertex_info.isPrimaryKey("firstName") == false)
+    assert(vertex_info.getFilePath(property_group_2, 0) == "vertex/person/firstName_lastName_gender/part0/chunk0")
+    assert(vertex_info.getFilePath(property_group_2, 4) == "vertex/person/firstName_lastName_gender/part4/chunk0")
+
+    assert(vertex_info.containProperty("not_exist") == false)
+    assertThrows[IllegalArgumentException](vertex_info.getPropertyGroup("not_exist"))
+    assertThrows[IllegalArgumentException](vertex_info.getPropertyType("now_exist"))
+    assertThrows[IllegalArgumentException](vertex_info.isPrimaryKey("now_exist"))
   }
 
+  
 }
