42cf0098549c864ea4a80e730298d2712e526b7c
有理 <youli.lx@alibaba-inc.com>
2023-01-17 11:18:06 +0800

    Add comments for TestReader

--- spark/src/test/scala/com/alibaba/graphar/TestReader.scala
+++ spark/src/test/scala/com/alibaba/graphar/TestReader.scala
@@ -32,7 +32,7 @@ class ReaderSuite extends AnyFunSuite {
     .getOrCreate()
 
   test("read chunk files directly") {
-    // read parquet files (vertex chunks)
+    // read vertex chunk files in Parquet
     val parquet_file_path = "gar-test/ldbc_sample/parquet"
     val parquet_prefix = getClass.getClassLoader.getResource(parquet_file_path).getPath
     val parqeut_read_path = parquet_prefix + "/vertex/person/id"
@@ -42,15 +42,15 @@ class ReaderSuite extends AnyFunSuite {
     assert(df1.count() == 903)
     //println(df1.rdd.collect().mkString("\n"))
 
-    // read orc files (vertex chunks)
+    // read vertex chunk files in Orc
     val orc_file_path = "gar-test/ldbc_sample/orc"
     val orc_prefix = getClass.getClassLoader.getResource(orc_file_path).getPath
     val orc_read_path = orc_prefix + "/vertex/person/id"
     val df2 = spark.read.option("fileFormat", "orc").format("com.alibaba.graphar.datasources.GarDataSource").load(orc_read_path)
-    // compare
+    // validate reading results
     assert(df2.rdd.collect().deep == df1.rdd.collect().deep)
 
-    // read csv files recursively (edge chunks)
+    // read adjList chunk files recursively in CSV
     val csv_file_path = "gar-test/ldbc_sample/csv"
     val csv_prefix = getClass.getClassLoader.getResource(csv_file_path).getPath
     val csv_read_path = csv_prefix + "/edge/person_knows_person/ordered_by_source/adj_list"
@@ -64,21 +64,31 @@ class ReaderSuite extends AnyFunSuite {
   }
 
   test("read vertex chunks") {
+    // construct the vertex information
     val file_path = "gar-test/ldbc_sample/csv"
     val prefix = getClass.getClassLoader.getResource(file_path).getPath
     val vertex_input = getClass.getClassLoader.getResourceAsStream(file_path + "/person.vertex.yml")
     val vertex_yaml = new Yaml(new Constructor(classOf[VertexInfo]))
     val vertex_info = vertex_yaml.load(vertex_input).asInstanceOf[VertexInfo]
 
+    // construct the vertex reader
     val reader = new VertexReader(prefix, vertex_info, spark)
+
+    // test reading the number of vertices
     assert(reader.readVerticesNumber() == 903)
     val property_group = vertex_info.getPropertyGroup("gender")
+
+    // test reading a single property chunk
     val single_chunk_df = reader.readVertexPropertyChunk(property_group, 0)
     assert(single_chunk_df.columns.size == 3)
     assert(single_chunk_df.count() == 100)
+
+    // test reading chunks for a property group
     val property_df = reader.readVertexProperties(property_group)
     assert(property_df.columns.size == 3)
     assert(property_df.count() == 903)
+
+    // test reading chunks for all property groups and optionally adding indices
     val vertex_df = reader.readAllVertexProperties()
     vertex_df.show()
     assert(vertex_df.columns.size == 4)
@@ -88,23 +98,30 @@ class ReaderSuite extends AnyFunSuite {
     assert(vertex_df_with_index.columns.size == 5)
     assert(vertex_df_with_index.count() == 903)
 
+    // throw an exception for non-existing property groups
     val invalid_property_group= new PropertyGroup()
     assertThrows[IllegalArgumentException](reader.readVertexPropertyChunk(invalid_property_group, 0))
     assertThrows[IllegalArgumentException](reader.readVertexProperties(invalid_property_group))
   }
 
   test("read edge chunks") {
+    // construct the edge information
     val file_path = "gar-test/ldbc_sample/csv"
     val prefix = getClass.getClassLoader.getResource(file_path).getPath
     val edge_input = getClass.getClassLoader.getResourceAsStream(file_path + "/person_knows_person.edge.yml")
     val edge_yaml = new Yaml(new Constructor(classOf[EdgeInfo]))
     val edge_info = edge_yaml.load(edge_input).asInstanceOf[EdgeInfo]
 
+    // construct the edge reader
     val adj_list_type = AdjListType.ordered_by_source
     val reader = new EdgeReader(prefix, edge_info, adj_list_type, spark)
+
+    // test reading a offset chunk
     val offset_df = reader.readOffset(0)
     assert(offset_df.columns.size == 1)
     assert(offset_df.count() == 101)
+
+    // test reading adjList chunks
     val single_adj_list_df = reader.readAdjListChunk(2, 0)
     assert(single_adj_list_df.columns.size == 2)
     assert(single_adj_list_df.count() == 1024)
@@ -115,6 +132,7 @@ class ReaderSuite extends AnyFunSuite {
     assert(adj_list_df.columns.size == 2)
     assert(adj_list_df.count() == 6626)
 
+    // test reading property group chunks
     val property_group = edge_info.getPropertyGroup("creationDate", adj_list_type)
     val single_property_df = reader.readEdgePropertyChunk(property_group, 2, 0)
     assert(single_property_df.columns.size == 1)
@@ -132,6 +150,7 @@ class ReaderSuite extends AnyFunSuite {
     assert(all_property_df.columns.size == 1)
     assert(all_property_df.count() == 6626)
 
+    // test reading edges and optionally adding indices
     val edge_df_chunk_2 = reader.readEdgesForVertexChunk(2)
     edge_df_chunk_2.show()
     assert(edge_df_chunk_2.columns.size == 3)
@@ -149,11 +168,13 @@ class ReaderSuite extends AnyFunSuite {
     assert(edge_df_with_index.columns.size == 4)
     assert(edge_df_with_index.count() == 6626)
 
+    // throw an exception for non-existing property groups
     val invalid_property_group= new PropertyGroup()
     assertThrows[IllegalArgumentException](reader.readEdgePropertyChunk(invalid_property_group, 0, 0))
     assertThrows[IllegalArgumentException](reader.readEdgePropertiesForVertexChunk(invalid_property_group, 0))
     assertThrows[IllegalArgumentException](reader.readEdgeProperties(invalid_property_group))
 
+    // throw an exception for non-existing adjList types
     val invalid_adj_list_type = AdjListType.unordered_by_dest
     assertThrows[IllegalArgumentException](new EdgeReader(prefix, edge_info, invalid_adj_list_type, spark))
   }
