463b4bdd3c490f77b62809ec66ba8f16c4857817
acezen <qiaozi.zwb@alibaba-inc.com>
2023-02-16 10:12:50 +0800

    Fix

--- spark/src/main/scala/com/alibaba/graphar/utils/FileSystem.scala
+++ spark/src/main/scala/com/alibaba/graphar/utils/FileSystem.scala
@@ -48,14 +48,14 @@ object FileSystem {
     }
     // write offset chunks data frame
     if (!offsetStartChunkIndex.isEmpty) {
-      return dataFrame.write.mode("append").option("fileFormat", fileType).option(GeneralParams.offsetStartChunkIndexKey, offsetStartChunkIndex.get).format("com.alibaba.graphar.datasources.GarDataSource").save(outputPrefix)
+      return dataFrame.write.mode("overwite").option("fileFormat", fileType).option(GeneralParams.offsetStartChunkIndexKey, offsetStartChunkIndex.get).format("com.alibaba.graphar.datasources.GarDataSource").save(outputPrefix)
     }
     // write edge chunks data frame
     if (!aggNumListOfEdgeChunk.isEmpty) {
       implicit val formats = DefaultFormats
-      return dataFrame.write.mode("append").option("fileFormat", fileType).option(GeneralParams.aggNumListOfEdgeChunkKey, write(aggNumListOfEdgeChunk.get)).format("com.alibaba.graphar.datasources.GarDataSource").save(outputPrefix)
+      return dataFrame.write.mode("overwite").option("fileFormat", fileType).option(GeneralParams.aggNumListOfEdgeChunkKey, write(aggNumListOfEdgeChunk.get)).format("com.alibaba.graphar.datasources.GarDataSource").save(outputPrefix)
     }
     // write vertex chunks data frame
-    dataFrame.write.mode("append").option("fileFormat", fileType).format("com.alibaba.graphar.datasources.GarDataSource").save(outputPrefix)
+    dataFrame.write.mode("overwite").option("fileFormat", fileType).format("com.alibaba.graphar.datasources.GarDataSource").save(outputPrefix)
   }
 }
