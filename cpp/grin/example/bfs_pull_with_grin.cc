3b06d6ad558ce9846efd5f6a712434bcb257dc39
有理 <youli.lx@alibaba-inc.com>
2023-04-26 14:49:20 +0800

    Add bfs_pull for testing performance

---
+++ cpp/grin/example/bfs_pull_with_grin.cc
@@ -0,0 +1,146 @@
+/** Copyright 2022 Alibaba Group Holding Limited.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <chrono>
+#include <iostream>
+#include <vector>
+
+#include "grin/test/config.h"
+
+#include "grin/include/index/order.h"
+#include "grin/include/property/property.h"
+#include "grin/include/property/propertytable.h"
+#include "grin/include/property/type.h"
+#include "grin/include/topology/adjacentlist.h"
+#include "grin/include/topology/edgelist.h"
+#include "grin/include/topology/structure.h"
+#include "grin/include/topology/vertexlist.h"
+
+void run_bfs(GRIN_GRAPH graph, size_t root = 0, bool print_result = false) {
+  std::cout << "++++ Run BFS (pull) algorithm with GRIN ++++" << std::endl;
+
+  // initialize parameters and the graph
+  const size_t num_vertices = grin_get_vertex_num(graph);
+  auto vertex_list = grin_get_vertex_list(graph);
+
+  // initialize BFS value of vertices
+  std::vector<int32_t> distance(num_vertices);
+  for (size_t i = 0; i < num_vertices; i++)
+    distance[i] = (i == root ? 0 : -1);
+
+  // run BFS algorithm
+  for (int iter = 0;; iter++) {
+    size_t count = 0;
+    for (size_t i = 0; i < num_vertices; i++) {
+      if (distance[i] == -1) {
+        // get vertex
+        auto v = grin_get_vertex_from_list(graph, vertex_list, i);
+
+        // get incoming edges of v and update it
+        auto adj_list = grin_get_adjacent_list(graph, GRIN_DIRECTION::IN, v);
+        auto it = grin_get_adjacent_list_begin(graph, adj_list);
+        while (grin_is_adjacent_list_end(graph, it) == false) {
+          // get neighbor
+          auto nbr = grin_get_neighbor_from_adjacent_list_iter(graph, it);
+          auto nbr_id = grin_get_position_of_vertex_from_sorted_list(
+              graph, vertex_list, nbr);
+          grin_destroy_vertex(graph, nbr);
+          // update
+          if (distance[nbr_id] == iter) {
+            distance[i] = iter + 1;
+            count++;
+            break;
+          }
+          grin_get_next_adjacent_list_iter(graph, it);
+        }
+
+        // destroy
+        grin_destroy_vertex(graph, v);
+        grin_destroy_adjacent_list(graph, adj_list);
+        grin_destroy_adjacent_list_iter(graph, it);
+      }
+    }
+    std::cout << "iter " << iter << ": " << count << " vertices." << std::endl;
+    if (count == 0)
+      break;
+  }
+
+  // output results
+  if (print_result) {
+    std::cout << "num_vertices: " << num_vertices << std::endl;
+    for (size_t i = 0; i < num_vertices; i++) {
+      // get vertex
+      auto v = grin_get_vertex_from_list(graph, vertex_list, i);
+
+      // get property "id" of vertex
+      auto type = grin_get_vertex_type(graph, v);
+      auto table = grin_get_vertex_property_table_by_type(graph, type);
+      auto property = grin_get_vertex_property_by_name(graph, type, "id");
+      auto data_type = grin_get_vertex_property_data_type(graph, property);
+      auto value =
+          grin_get_value_from_vertex_property_table(graph, table, v, property);
+
+      // output
+      std::cout << "vertex " << i;
+      if (data_type == GRIN_DATATYPE::Int64) {
+        std::cout << ", id = " << *static_cast<const int64_t*>(value);
+      }
+      std::cout << ", distance = " << distance[i] << std::endl;
+
+      // destroy
+      grin_destroy_value(graph, data_type, value);
+      grin_destroy_vertex_property(graph, property);
+      grin_destroy_vertex_property_table(graph, table);
+      grin_destroy_vertex_type(graph, type);
+      grin_destroy_vertex(graph, v);
+    }
+  }
+
+  grin_destroy_vertex_list(graph, vertex_list);
+
+  std::cout << "---- Run BFS (pull) algorithm completed ----" << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+  // get graph from graph info of GraphAr
+  std::string path = TEST_DATA_SMALL_PATH;
+  std::cout << "GraphInfo path = " << path << std::endl;
+
+  char** args = new char*[1];
+  args[0] = new char[path.length() + 1];
+  snprintf(args[0], path.length() + 1, "%s", path.c_str());
+  auto init_start = std::chrono::high_resolution_clock::now();
+  GRIN_GRAPH graph = grin_get_graph_from_storage(1, args);
+  auto init_end = std::chrono::high_resolution_clock::now();
+  auto init_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+      init_end - init_start);
+
+  // run pagerank algorithm
+  auto run_start = std::chrono::high_resolution_clock::now();
+  run_bfs(graph);
+  auto run_end = std::chrono::high_resolution_clock::now();
+  auto run_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+      run_end - run_start);
+
+  // output execution time
+  std::cout << "Init time for BFS (pull) with GRIN = " << init_time.count()
+            << " ms" << std::endl;
+  std::cout << "Run time for BFS (pull) with GRIN = " << run_time.count()
+            << " ms" << std::endl;
+  std::cout << "Totoal time for BFS (pull) with GRIN = "
+            << init_time.count() + run_time.count() << " ms" << std::endl;
+
+  return 0;
+}
---
+++ cpp/grin/example/bfs_pull_without_grin.cc
@@ -0,0 +1,106 @@
+/** Copyright 2022 Alibaba Group Holding Limited.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <chrono>
+#include <iostream>
+
+#include "gar/graph.h"
+#include "gar/graph_info.h"
+#include "gar/reader/arrow_chunk_reader.h"
+#include "gar/writer/arrow_chunk_writer.h"
+
+#include "grin/test/config.h"
+
+void run_bfs(GAR_NAMESPACE::GraphInfo graph_info,
+             GAR_NAMESPACE::IdType root = 0, bool print_result = false) {
+  std::cout << "++++ Run BFS (pull) algorithm without GRIN ++++" << std::endl;
+
+  // construct vertices collection
+  std::string label = "person";
+  assert(graph_info.GetVertexInfo(label).status().ok());
+  auto maybe_vertices =
+      GAR_NAMESPACE::ConstructVerticesCollection(graph_info, label);
+  assert(maybe_vertices.status().ok());
+  auto& vertices = maybe_vertices.value();
+  int num_vertices = vertices.size();
+
+  // construct edges collection
+  std::string src_label = "person", edge_label = "knows", dst_label = "person";
+  auto maybe_edges = GAR_NAMESPACE::ConstructEdgesCollection(
+      graph_info, src_label, edge_label, dst_label,
+      GAR_NAMESPACE::AdjListType::ordered_by_dest);
+  assert(!maybe_edges.has_error());
+  auto& edges = std::get<GAR_NAMESPACE::EdgesCollection<
+      GAR_NAMESPACE::AdjListType::ordered_by_dest>>(maybe_edges.value());
+
+  // initialize distance
+  std::vector<int32_t> distance(num_vertices);
+  for (GAR_NAMESPACE::IdType i = 0; i < num_vertices; i++)
+    distance[i] = (i == root ? 0 : -1);
+
+  // run bfs algorithm
+  for (int iter = 0;; iter++) {
+    GAR_NAMESPACE::IdType count = 0;
+    for (GAR_NAMESPACE::IdType vid = 0; vid < num_vertices; vid++) {
+      if (distance[vid] == -1) {
+        auto it = edges.find_dst(vid, edges.begin());
+        if (it == edges.end())
+          continue;
+        do {
+          GAR_NAMESPACE::IdType nbr = it.source();
+          if (distance[nbr] == iter) {
+            distance[vid] = iter + 1;
+            count++;
+            break;
+          }
+        } while (it.next_dst());
+      }
+    }
+    std::cout << "iter " << iter << ": " << count << " vertices." << std::endl;
+    if (count == 0)
+      break;
+  }
+
+  // output results
+  if (print_result) {
+    std::cout << "num_vertices: " << num_vertices << std::endl;
+    auto it = vertices.begin();
+    for (size_t i = 0; i < num_vertices; i++) {
+      std::cout << "vertex " << i
+                << ", id = " << it.property<int64_t>("id").value()
+                << ", distance = " << distance[i] << std::endl;
+      ++it;
+    }
+  }
+
+  std::cout << "---- Run BFS (pull) algorithm completed ----" << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+  // read file and construct graph info
+  std::string path = TEST_DATA_SMALL_PATH;
+  std::cout << "GraphInfo path = " << path << std::endl;
+  auto graph_info = GAR_NAMESPACE::GraphInfo::Load(path).value();
+
+  // run BFS (push) algorithm
+  auto run_start = std::chrono::high_resolution_clock::now();
+  run_bfs(graph_info);
+  auto run_end = std::chrono::high_resolution_clock::now();
+  auto run_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+      run_end - run_start);
+
+  std::cout << "Run time for BFS (pull) without GRIN = " << run_time.count()
+            << " ms" << std::endl;
+}
