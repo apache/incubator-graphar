6894f494c1bf29dd2a647ff18d10fbf6c040edc8
有理 <youli.lx@alibaba-inc.com>
2023-06-27 15:09:09 +0800

    Update header files

--- cpp/grin/src/common/message.cc
+++ cpp/grin/src/common/message.cc
@@ -311,6 +311,14 @@ const char* grin_get_static_storage_feature_msg() {
 #ifdef GRIN_ENABLE_VERTEX_INTERNAL_ID_INDEX
     feature->set_grin_enable_vertex_internal_id_index(true);
 #endif
+
+#ifdef GRIN_ENABLE_VERTEX_PK_INDEX
+    feature->set_grin_enable_vertex_pk_index(true);
+#endif
+
+#ifdef GRIN_ENABLE_EDGE_PK_INDEX
+    feature->set_grin_enable_edge_pk_index(true);
+#endif
   }
 
   std::string graph_def;
