0e385232ae81004698a1f0aac0c32a6a03de862f
有理 <youli.lx@alibaba-inc.com>
2023-10-18 15:19:53 +0800

    simplify high-level writer

--- cpp/examples/high_level_writer_example.cc
+++ cpp/examples/high_level_writer_example.cc
@@ -16,9 +16,7 @@ limitations under the License.
 #include <iostream>
 
 #include "arrow/api.h"
-#include "arrow/csv/api.h"
 #include "arrow/filesystem/api.h"
-#include "parquet/arrow/reader.h"
 
 #include "./config.h"
 #include "gar/writer/edges_builder.h"
@@ -37,39 +35,24 @@ void vertices_builder() {
   // set validate level
   builder.SetValidateLevel(GAR_NAMESPACE::ValidateLevel::strong_validate);
 
-  // read data from a csv file
-  std::ifstream fp(TEST_DATA_DIR + "/ldbc_sample/person_0_0.csv");
-  std::string line;
-  getline(fp, line);
-  int m = 4;
-  std::vector<std::string> names;
-  std::istringstream readstr(line);
-  for (int i = 0; i < m; i++) {
-    std::string name;
-    getline(readstr, name, '|');
-    names.push_back(name);
-  }
+  // prepare vertex data
+  int vertex_count = 3;
+  std::vector<std::string> property_names = {"id", "firstName", "lastName"};
+  std::vector<int64_t> id = {0, 1, 2};
+  std::vector<std::string> firstName = {"John", "Jane", "Alice"};
+  std::vector<std::string> lastName = {"Smith", "Doe", "Xu"};
 
-  // read data and add vertices
-  while (getline(fp, line)) {
-    std::string val;
-    std::istringstream readstr(line);
+  // add vertices
+  for (int i = 0; i < vertex_count; i++) {
     GAR_NAMESPACE::builder::Vertex v;
-    for (int i = 0; i < m; i++) {
-      getline(readstr, val, '|');
-      if (i == 0) {
-        int64_t x = 0;
-        for (size_t j = 0; j < val.length(); j++)
-          x = x * 10 + val[j] - '0';
-        v.AddProperty(names[i], x);
-      } else {
-        v.AddProperty(names[i], val);
-      }
-    }
+    v.AddProperty(property_names[0], id[i]);
+    v.AddProperty(property_names[1], firstName[i]);
+    v.AddProperty(property_names[2], lastName[i]);
     ASSERT(builder.AddVertex(v).ok());
   }
 
   // dump
+  ASSERT(builder.GetNum() == vertex_count);
   std::cout << "vertex_count=" << builder.GetNum() << std::endl;
   ASSERT(builder.Dump().ok());
   std::cout << "dump vertices collection successfully!" << std::endl;
@@ -85,47 +68,31 @@ void edges_builder() {
       TEST_DATA_DIR + "/ldbc_sample/parquet/" + "person_knows_person.edge.yml";
   auto edge_meta = GAR_NAMESPACE::Yaml::LoadFile(edge_meta_file).value();
   auto edge_info = GAR_NAMESPACE::EdgeInfo::Load(edge_meta).value();
-  auto vertices_num = 903;
+  auto vertex_count = 3;
   GAR_NAMESPACE::builder::EdgesBuilder builder(
       edge_info, "/tmp/", GraphArchive::AdjListType::ordered_by_dest,
-      vertices_num);
+      vertex_count);
 
   // set validate level
   builder.SetValidateLevel(GAR_NAMESPACE::ValidateLevel::strong_validate);
 
-  // read data from a csv file
-  std::ifstream fp(TEST_DATA_DIR + "/ldbc_sample/person_knows_person_0_0.csv");
-  std::string line;
-  getline(fp, line);
-  std::vector<std::string> names;
-  std::istringstream readstr(line);
-  std::map<std::string, int64_t> mapping;
-  int64_t cnt = 0;
+  // prepare edge data
+  int edge_count = 4;
+  std::vector<std::string> property_names = {"creationDate"};
+  std::vector<int64_t> src = {1, 0, 0, 2};
+  std::vector<int64_t> dst = {0, 1, 2, 1};
+  std::vector<std::string> creationDate = {"2010-01-01", "2011-01-01",
+                                           "2012-01-01", "2013-01-01"};
 
-  // read data and add edges
-  while (getline(fp, line)) {
-    std::string val;
-    std::istringstream readstr(line);
-    int64_t s = 0, d = 0;
-    for (int i = 0; i < 3; i++) {
-      getline(readstr, val, '|');
-      if (i == 0) {
-        if (mapping.find(val) == mapping.end())
-          mapping[val] = cnt++;
-        s = mapping[val];
-      } else if (i == 1) {
-        if (mapping.find(val) == mapping.end())
-          mapping[val] = cnt++;
-        d = mapping[val];
-      } else {
-        GAR_NAMESPACE::builder::Edge e(s, d);
-        e.AddProperty("creationDate", val);
-        ASSERT(builder.AddEdge(e).ok());
-      }
-    }
+  // add edges
+  for (int i = 0; i < edge_count; i++) {
+    GAR_NAMESPACE::builder::Edge e(src[i], dst[i]);
+    e.AddProperty("creationDate", creationDate[i]);
+    ASSERT(builder.AddEdge(e).ok());
   }
 
   // dump
+  ASSERT(builder.GetNum() == edge_count);
   std::cout << "edge_count=" << builder.GetNum() << std::endl;
   ASSERT(builder.Dump().ok());
   std::cout << "dump edges collection successfully!" << std::endl;
