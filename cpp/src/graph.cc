80f920c0ef1c569273c55140ac2887cdef05ff41
acezen <qiaozi.zwb@alibaba-inc.com>
2023-11-20 21:28:46 +0800

    Fix

--- cpp/src/graph.cc
+++ cpp/src/graph.cc
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include <iostream>
 #include "gar/graph.h"
 #include "gar/util/convert_to_arrow_type.h"
 
