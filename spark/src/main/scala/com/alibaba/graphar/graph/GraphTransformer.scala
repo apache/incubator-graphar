3204a35f0039a45ca31d9e0da281cba5a7070c19
acezen <qiaozi.zwb@alibaba-inc.com>
2024-01-18 11:26:17 +0800

    Add persist in transformer

--- spark/src/main/scala/com/alibaba/graphar/graph/GraphTransformer.scala
+++ spark/src/main/scala/com/alibaba/graphar/graph/GraphTransformer.scala
@@ -67,9 +67,11 @@ object GraphTransformer {
       // read vertex chunks from the source graph
       val reader = new VertexReader(source_prefix, source_vertex_info, spark)
       val df = reader.readAllVertexPropertyGroups()
+      df.persist(GeneralParams.defaultStorageLevel)
       // write vertex chunks for the dest graph
       val writer = new VertexWriter(dest_prefix, dest_vertex_info, df)
       writer.writeVertexProperties()
+      df.unpersist()
     }
   }
 
@@ -138,6 +140,7 @@ object GraphTransformer {
           )
           df = reader.readEdges(false)
           has_loaded = true
+          df.persist(GeneralParams.defaultStorageLevel)
         }
 
         // read vertices number
@@ -167,6 +170,7 @@ object GraphTransformer {
           df
         )
         writer.writeEdges()
+        df.unpersist()
       }
     }
   }
