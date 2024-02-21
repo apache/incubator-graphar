16c7a0878e93cd49ceb7b77646838cf160e377c9
SemyonSinchenko <semyonsinchenko@yahoo.com>
2024-02-22 00:25:23 +0100

    Fix license ignore && fix broken path to GraphAr JAR
     On branch 320-datasources-refactoring
     Changes to be committed:
    	modified:   .licenserc.yaml
    	modified:   spark/scripts/run-graphar2nebula.sh
    	modified:   spark/scripts/run-graphar2neo4j.sh
    	modified:   spark/scripts/run-nebula2graphar.sh
    	modified:   spark/scripts/run-neo4j2graphar.sh

--- spark/scripts/run-graphar2nebula.sh
+++ spark/scripts/run-graphar2nebula.sh
@@ -16,7 +16,7 @@
 set -eu
 
 cur_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-jar_file="${cur_dir}/../target/graphar-0.1.0-SNAPSHOT-shaded.jar"
+jar_file="${cur_dir}/../graphar/target/graphar-commons-0.1.0-SNAPSHOT-shaded.jar"
 
 graph_info_path="${GRAPH_INFO_PATH:-/tmp/graphar/nebula2graphar/basketballplayergraph.graph.yml}"
 spark-submit --class com.alibaba.graphar.example.GraphAr2Nebula ${jar_file} \
