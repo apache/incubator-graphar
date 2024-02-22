bdcf3677a85ae41437d15a525226c1e409033f46
Semyon <semyonsinchenko@yahoo.com>
2024-02-23 00:50:40 +0100

    [Feat][Spark] Split datasources and core, prepare for support of multiple spark versions (#369)
    * Refactoring of the GraphAr Spark
    
    - split datasources and core GraphAr
    - introduce Maven profiles for different versions of Spark
    - small fixes of PySpark part due to new naming and paths
    - new pom.xml files for subprojects

--- spark/scripts/run-graphar2neo4j.sh
+++ spark/scripts/run-graphar2neo4j.sh
@@ -17,7 +17,7 @@
 set -eu
 
 cur_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-jar_file="${cur_dir}/../target/graphar-0.1.0-SNAPSHOT-shaded.jar"
+jar_file="${cur_dir}/../graphar/target/graphar-commons-0.1.0-SNAPSHOT-shaded.jar"
 
 graph_info_path="${GRAPH_INFO_PATH:-/tmp/graphar/neo4j2graphar/MovieGraph.graph.yml}"
 spark-submit --class com.alibaba.graphar.example.GraphAr2Neo4j ${jar_file} \
