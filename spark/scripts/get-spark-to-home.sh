cd5b91d98afd71969eeeaad5ad07780f83e4ff53
SemyonSinchenko <semyonsinchenko@yahoo.com>
2024-02-24 11:55:16 +0100

    Fix downloading of spark
     On branch 319-spark-33x
     Changes to be committed:
    	modified:   .github/workflows/spark.yaml
    	modified:   spark/scripts/get-spark-to-home.sh

--- spark/scripts/get-spark-to-home.sh
+++ spark/scripts/get-spark-to-home.sh
@@ -17,4 +17,4 @@
 set -eu
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-curl https://archive.apache.org/dist/spark/spark-3.2.2/spark-3.2.2-bin-hadoop3.2.tgz | tar -xz -C ${HOME}/
+curl https://archive.apache.org/dist/spark/${1}/${2}.tgz | tar -xz -C ${HOME}/
