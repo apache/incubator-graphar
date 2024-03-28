46a173f53863c89c45c1418a4de8cfd7f6df68cf
acezen <qiaozi.zwb@alibaba-inc.com>
2024-03-28 19:16:57 +0800

    Fix error
    Signed-off-by: acezen <qiaozi.zwb@alibaba-inc.com>

--- java/src/main/cpp/ffi/jni_com_alibaba_graphar_arrow_ArrowTable_Static.cc
+++ java/src/main/cpp/ffi/jni_com_alibaba_graphar_arrow_ArrowTable_Static.cc
@@ -30,7 +30,7 @@ extern "C" {
 // Common Stubs
 
 JNIEXPORT
-jlong JNICALL Java_com_alibaba_graphar_arrow_ArrowTable_1Static_1cxx_10x58c7409_nativeFromArrowArrayAndArrowSchema(JNIEnv*, jclass, jlong rv_base, jlong arg0 /* arrayAddress0 */, jlong arg1 /* schemaAddress1 */) {
+jlong JNICALL Java_org_apache_graphar_arrow_ArrowTable_1Static_1cxx_10x58c7409_nativeFromArrowArrayAndArrowSchema(JNIEnv*, jclass, jlong rv_base, jlong arg0 /* arrayAddress0 */, jlong arg1 /* schemaAddress1 */) {
     auto maybeRecordBatch = arrow::ImportRecordBatch(reinterpret_cast<struct ArrowArray*>(arg0), reinterpret_cast<struct ArrowSchema*>(arg1));
     auto table = arrow::Table::FromRecordBatches({maybeRecordBatch.ValueOrDie()});
 	return reinterpret_cast<jlong>(new((void*)rv_base) arrow::Result<std::shared_ptr<arrow::Table>>(table));
