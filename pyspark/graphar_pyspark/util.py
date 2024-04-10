98cdeed42e5e0ce4e4e16097b1eb1f70f2defee4
Weibin Zeng <qiaozi.zwb@alibaba-inc.com>
2024-04-10 14:26:40 +0800

    chore: fix the header of pyspark source code (#446)
    Signed-off-by: acezen <qiaozi.zwb@alibaba-inc.com>

--- pyspark/graphar_pyspark/util.py
+++ pyspark/graphar_pyspark/util.py
@@ -15,20 +15,6 @@
 # specific language governing permissions and limitations
 # under the License.
 
-# copyright 2022-2023 alibaba group holding limited.
-#
-# licensed under the apache license, version 2.0 (the "license");
-# you may not use this file except in compliance with the license.
-# you may obtain a copy of the license at
-#
-#     http://www.apache.org/licenses/license-2.0
-#
-# unless required by applicable law or agreed to in writing, software
-# distributed under the license is distributed on an "as is" basis,
-# without warranties or conditions of any kind, either express or implied.
-# see the license for the specific language governing permissions and
-# limitations under the license.
-
 """Bindings to org.apache.graphar.util."""
 
 from __future__ import annotations
