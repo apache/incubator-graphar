09f6aca46a0e01d87368a6471e688e40aeec3b18
acezen <qiaozi.zwb@alibaba-inc.com>
2024-05-20 15:13:42 +0800

    fix(license): merging ASF licence header and Alibaba Group copyright if the code has been modified on top of reference code
    Signed-off-by: acezen <qiaozi.zwb@alibaba-inc.com>

--- maven-projects/java/src/main/java/org/apache/graphar/stdcxx/StdString.java
+++ maven-projects/java/src/main/java/org/apache/graphar/stdcxx/StdString.java
@@ -1,18 +1,18 @@
 /*
- * Copyright 1999-2021 Alibaba Group Holding Ltd.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   https://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This file is part of the Apache GraphAr (incubating) project, which was derived from fastFFI.
  */
+
+/*
+ * Copyright 1999-2021 Alibaba Group Holding Ltd.
+ */
+
+// Derived from alibaba/fastFFI v0.1.2
+// https://github.com/alibaba/fastFFI/blob/1eca42b/llvm/src/main/java/com/alibaba/fastffi/stdcxx/StdString.java
 
 package org.apache.graphar.stdcxx;
 
