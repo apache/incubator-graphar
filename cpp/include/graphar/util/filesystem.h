77fb366a475035f3d470a8808c2ae2970158ad7e
teapot1de <121592812+ywh555hhh@users.noreply.github.com>
2024-05-22 19:42:59 +0800

    Update filesystem.h

--- cpp/include/graphar/util/filesystem.h
+++ cpp/include/graphar/util/filesystem.h
@@ -35,19 +35,19 @@
 namespace arrow {
 class Buffer;
 class Table;
-}// namespace arrow
+}  // namespace arrow
 
 namespace arrow::fs {
 class FileSystem;
-}// namespace arrow::fs
+}  // namespace arrow::fs
 
 namespace arrow::io {
 class RandomAccessFile;
-}// namespace arrow::io
+}  // namespace arrow::io
 
 namespace arrow::dataset {
 class FileFormat;
-} // namespace arrow::dataset
+}  // namespace arrow::dataset
 
 namespace graphar {
 
