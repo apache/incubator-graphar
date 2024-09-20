7de66d1ac9ae104c9676675c46546b1e9900388f
acezen <qiaozi.zwb@alibaba-inc.com>
2024-09-20 11:36:28 +0800

    Update
    Signed-off-by: acezen <qiaozi.zwb@alibaba-inc.com>

--- scripts/6.5/gen-ic8-data.sh
+++ scripts/6.5/gen-ic8-data.sh
@@ -3,26 +3,26 @@
 # Generate the base dataset
 # ./release/Csv2Parquet-is3 sourcefile output_file_prefix ignore_rows type [to_undirected]
 #!/bin/bash
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/person_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf30/social_network/parquet/person_0_0 0 vertex
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/comment_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf30/social_network/parquet/comment_0_0 0 vertex
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/post_hasCreator_person_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf30/social_network/parquet/post_hasCreator_person_0_0 0 edge
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/comment_hasCreator_person_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf30/social_network/parquet/comment_hasCreator_person_0_0 0 edge
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/comment_replyOf_post_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf30/social_network/parquet/comment_replyOf_post_0_0 0 edge
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/comment_replyOf_comment_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf30/social_network/parquet/comment_replyOf_comment_0_0 0 edge
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/person_0_0.csv /tmp/sf30/person_0_0 0 vertex
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/comment_0_0.csv /tmp/sf30/comment_0_0 0 vertex
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/post_hasCreator_person_0_0.csv /tmp/sf30/post_hasCreator_person_0_0 0 edge
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/comment_hasCreator_person_0_0.csv /tmp/sf30/comment_hasCreator_person_0_0 0 edge
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/comment_replyOf_post_0_0.csv /tmp/sf30/comment_replyOf_post_0_0 0 edge
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/comment_replyOf_comment_0_0.csv /tmp/sf30/comment_replyOf_comment_0_0 0 edge
 # sf100
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/person_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf100/social_network/parquet/person_0_0 0 vertex
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/comment_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf100/social_network/parquet/comment_0_0 0 vertex
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/post_hasCreator_person_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf100/social_network/parquet/post_hasCreator_person_0_0 0 edge
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/comment_hasCreator_person_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf100/social_network/parquet/comment_hasCreator_person_0_0 0 edge
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/comment_replyOf_post_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf100/social_network/parquet/comment_replyOf_post_0_0 0 edge
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/comment_replyOf_comment_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf100/social_network/parquet/comment_replyOf_comment_0_0 0 edge
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/person_0_0.csv /tmp/sf100/person_0_0 0 vertex
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/comment_0_0.csv /tmp/sf100/comment_0_0 0 vertex
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/post_hasCreator_person_0_0.csv /tmp/sf100/post_hasCreator_person_0_0 0 edge
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/comment_hasCreator_person_0_0.csv /tmp/sf100/comment_hasCreator_person_0_0 0 edge
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/comment_replyOf_post_0_0.csv /tmp/sf100/comment_replyOf_post_0_0 0 edge
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/comment_replyOf_comment_0_0.csv /tmp/sf100/comment_replyOf_comment_0_0 0 edge
 # sf300
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/person_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf300/social_network/parquet/person_0_0 0 vertex
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/comment_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf300/social_network/parquet/comment_0_0 0 vertex
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/post_hasCreator_person_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf300/social_network/parquet/post_hasCreator_person_0_0 0 edge
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/comment_hasCreator_person_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf300/social_network/parquet/comment_hasCreator_person_0_0 0 edge
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/comment_replyOf_post_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf300/social_network/parquet/comment_replyOf_post_0_0 0 edge
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/comment_replyOf_comment_0_0.csv /mnt/ldbc/mengke.mk/datagen/sf300/social_network/parquet/comment_replyOf_comment_0_0 0 edge
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/person_0_0.csv /tmp/sf300/person_0_0 0 vertex
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/comment_0_0.csv /tmp/sf300/comment_0_0 0 vertex
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/post_hasCreator_person_0_0.csv /tmp/sf300/post_hasCreator_person_0_0 0 edge
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/comment_hasCreator_person_0_0.csv /tmp/sf300/comment_hasCreator_person_0_0 0 edge
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/comment_replyOf_post_0_0.csv /tmp/sf300/comment_replyOf_post_0_0 0 edge
+./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/comment_replyOf_comment_0_0.csv /tmp/sf300/comment_replyOf_comment_0_0 0 edge
 
 # Generate the graphar dataset
 # ./data-generator-label edge_source_file src_vertex_source_file dst_vertex_source_file src_label dst_label src_file_type dst_file_type output_file_prefix reverse/not-reverse to_undirected/not_to_undirected [just_vertex]
