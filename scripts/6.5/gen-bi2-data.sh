b1d7e6856ad2ee3a24f99e5b1eae0bbbfbf5edbf
acezen <qiaozi.zwb@alibaba-inc.com>
2024-09-20 11:54:32 +0800

    Update
    Signed-off-by: acezen <qiaozi.zwb@alibaba-inc.com>

--- scripts/6.5/gen-bi2-data.sh
+++ scripts/6.5/gen-bi2-data.sh
@@ -1,19 +1,19 @@
 !/bin/bash
 
 # Generate the base dataset
-# ./release/Csv2Parquet-is3 sourcefile output_file_prefix ignore_rows type [to_undirected]
+# ./release/Csv2Parquet-ldbc sourcefile output_file_prefix ignore_rows type [to_undirected]
 #!/bin/bash
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/post_0_0.csv /tmp/sf30/bi/post_0_0 0 vertex
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/post_hasTag_tag_0_0.csv /tmp/sf30/bi/post_hasTag_tag_0_0 0 edge
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/comment_hasTag_tag_0_0.csv /tmp/sf30/bi/comment_hasTag_tag_0_0 0 edge
+./release/Csv2Parquet-ldbc /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/post_0_0.csv /tmp/sf30/bi/post_0_0 0 vertex
+./release/Csv2Parquet-ldbc /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/post_hasTag_tag_0_0.csv /tmp/sf30/bi/post_hasTag_tag_0_0 0 edge
+./release/Csv2Parquet-ldbc /mnt/ldbc/mengke.mk/datagen/sf30/social_network/dynamic/comment_hasTag_tag_0_0.csv /tmp/sf30/bi/comment_hasTag_tag_0_0 0 edge
 # sf100
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/post_0_0.csv /tmp/sf100/bi/post_0_0 0 vertex
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/post_hasTag_tag_0_0.csv /tmp/sf100/bi/post_hasTag_tag_0_0 0 edge
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/comment_hasTag_tag_0_0.csv /tmp/sf100/bi/comment_hasTag_tag_0_0 0 edge
+./release/Csv2Parquet-ldbc /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/post_0_0.csv /tmp/sf100/bi/post_0_0 0 vertex
+./release/Csv2Parquet-ldbc /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/post_hasTag_tag_0_0.csv /tmp/sf100/bi/post_hasTag_tag_0_0 0 edge
+./release/Csv2Parquet-ldbc /mnt/ldbc/mengke.mk/datagen/sf100/social_network/dynamic/comment_hasTag_tag_0_0.csv /tmp/sf100/bi/comment_hasTag_tag_0_0 0 edge
 # sf300
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/post_0_0.csv /tmp/sf300/bi/post_0_0 0 vertex
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/post_hasTag_tag_0_0.csv /tmp/sf300/bi/post_hasTag_tag_0_0 0 edge
-./release/Csv2Parquet-is3 /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/comment_hasTag_tag_0_0.csv /tmp/sf300/bi/comment_hasTag_tag_0_0 0 edge
+./release/Csv2Parquet-ldbc /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/post_0_0.csv /tmp/sf300/bi/post_0_0 0 vertex
+./release/Csv2Parquet-ldbc /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/post_hasTag_tag_0_0.csv /tmp/sf300/bi/post_hasTag_tag_0_0 0 edge
+./release/Csv2Parquet-ldbc /mnt/ldbc/mengke.mk/datagen/sf300/social_network/dynamic/comment_hasTag_tag_0_0.csv /tmp/sf300/bi/comment_hasTag_tag_0_0 0 edge
 
 # Generate the graphar dataset
 # ./data-generator-label edge_source_file src_vertex_source_file dst_vertex_source_file src_label dst_label src_file_type dst_file_type output_file_prefix reverse/not-reverse to_undirected/not_to_undirected [just_vertex]
