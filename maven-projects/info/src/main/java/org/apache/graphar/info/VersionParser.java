047153ca07291e4c22192cb0bb983e0c18333abb
Selim Soufargi <ssoufargi.idealab.unical@gmail.com~>
2025-08-06 09:42:33 +0200

    test bug fix format

--- maven-projects/info/src/main/java/org/apache/graphar/info/VersionParser.java
+++ maven-projects/info/src/main/java/org/apache/graphar/info/VersionParser.java
@@ -27,7 +27,7 @@ import java.util.regex.Pattern;
 public class VersionParser {
     public static VersionInfo getVersion(String versionStr) {
         if (versionStr == "" || versionStr == null) {
-            return new VersionInfo("",new ArrayList<>());
+            return new VersionInfo("", new ArrayList<>());
         }
         try {
 
