d2eda0633bc09c9f811f515b2ff396cc1f9c16ba
Selim Soufargi <ssoufargi.idealab.unical@gmail.com~>
2025-08-11 09:46:24 +0200

    versionInfo version int is not string bug

--- maven-projects/info/src/main/java/org/apache/graphar/info/VersionParser.java
+++ maven-projects/info/src/main/java/org/apache/graphar/info/VersionParser.java
@@ -27,7 +27,7 @@ import java.util.regex.Pattern;
 public class VersionParser {
     public static VersionInfo getVersion(String versionStr) {
         if (versionStr == "" || versionStr == null) {
-            return new VersionInfo("", new ArrayList<>());
+            return new VersionInfo(0, new ArrayList<>());
         }
         try {
 
