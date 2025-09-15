129613ae1ff15388b3f5c4b9553f6e091cb45798
Jarvx200 <jarvx404@gmail.com>
2025-09-15 22:32:08 +0300

    test: Added basic testing for new `VertexInfoBuilder` and null checking

---
+++ maven-projects/info/src/test/java/org/apache/graphar/info/VertexInfoTest.java
@@ -0,0 +1,32 @@
+package org.apache.graphar.info;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class VertexInfoTest {
+
+    private VertexInfo.VertexInfoBuilder b =  VertexInfo.builder()
+            .baseUri("test")
+            .type("test")
+            .chunkSize(24)
+            .version("gar/v1")
+            .propertyGroups(new PropertyGroups(List.of(TestUtil.pg3)));
+
+    @Test
+    public void testVertexInfoBasicBuilder(){
+        VertexInfo v = b.build();
+    }
+
+    @Test
+    public void testVertexInfoBuilderDoubleDeclaration() throws URISyntaxException {
+        VertexInfo v =
+                b.baseUri(new URI("world"))
+                        .build();
+
+                 Assert.assertEquals(new URI("test"), v.getBaseUri());
+    }
+}
