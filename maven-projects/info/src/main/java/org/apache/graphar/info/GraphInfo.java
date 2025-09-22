f9f1816bed340b0613a7551b36c04bb7b8ff977e
yxk485490 <1320342065@qq.com>
2025-09-22 10:38:17 +0800

    fix import

--- maven-projects/info/src/main/java/org/apache/graphar/info/GraphInfo.java
+++ maven-projects/info/src/main/java/org/apache/graphar/info/GraphInfo.java
@@ -20,6 +20,8 @@
 package org.apache.graphar.info;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
