27d9bc8fbc2d95f6375d6e3324ed66c30d5a3c19
yxk485490 <1320342065@qq.com>
2025-09-28 11:21:41 +0800

    fix

--- maven-projects/info/src/main/java/org/apache/graphar/info/loader/StreamGraphInfoLoader.java
+++ maven-projects/info/src/main/java/org/apache/graphar/info/loader/StreamGraphInfoLoader.java
@@ -48,9 +48,9 @@ public abstract class StreamGraphInfoLoader extends BaseGraphInfoLoader {
     @Override
     public VertexInfo loadVertexInfo(URI vertexYamlUri) throws IOException {
         InputStream yaml = readYaml(vertexYamlUri);
-        Yaml edgeYamlLoader = new Yaml(new Constructor(VertexYaml.class, new LoaderOptions()));
-        VertexYaml edgeYaml = edgeYamlLoader.load(yaml);
-        return buildVertexInfoFromVertexYaml(edgeYaml);
+        Yaml vertexYamlLoader = new Yaml(new Constructor(VertexYaml.class, new LoaderOptions()));
+        VertexYaml vertexYaml = vertexYamlLoader.load(yaml);
+        return buildVertexInfoFromVertexYaml(vertexYaml);
     }
 
     @Override
