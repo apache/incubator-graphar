02bd3fe10a72436f21cf1da889172159273ae224
姚军 <940334249@qq.com>
2026-02-10 19:04:41 +0800

    fix(format): add clang-format to check all c++ files and fix exist format problem (#847)

--- maven-projects/java/src/main/cpp/ffi/jni_org_apache_graphar_arrow_ArrowTableStatic.cc
+++ maven-projects/java/src/main/cpp/ffi/jni_org_apache_graphar_arrow_ArrowTableStatic.cc
@@ -17,11 +17,11 @@
  * under the License.
  */
 
-#include <jni.h>
-#include <new>
-#include <iostream>
 #include "arrow/api.h"
 #include "arrow/c/bridge.h"
+#include <iostream>
+#include <jni.h>
+#include <new>
 
 #ifdef __cplusplus
 extern "C" {
@@ -30,10 +30,16 @@ extern "C" {
 // Common Stubs
 
 JNIEXPORT
-jlong JNICALL Java_org_apache_graphar_arrow_ArrowTable_1Static_1cxx_10x58c7409_nativeFromArrowArrayAndArrowSchema(JNIEnv*, jclass, jlong rv_base, jlong arg0 /* arrayAddress0 */, jlong arg1 /* schemaAddress1 */) {
-    auto maybeRecordBatch = arrow::ImportRecordBatch(reinterpret_cast<struct ArrowArray*>(arg0), reinterpret_cast<struct ArrowSchema*>(arg1));
-    auto table = arrow::Table::FromRecordBatches({maybeRecordBatch.ValueOrDie()});
-	return reinterpret_cast<jlong>(new((void*)rv_base) arrow::Result<std::shared_ptr<arrow::Table>>(table));
+jlong JNICALL
+Java_org_apache_graphar_arrow_ArrowTable_1Static_1cxx_10x58c7409_nativeFromArrowArrayAndArrowSchema(
+    JNIEnv *, jclass, jlong rv_base, jlong arg0 /* arrayAddress0 */,
+    jlong arg1 /* schemaAddress1 */) {
+  auto maybeRecordBatch =
+      arrow::ImportRecordBatch(reinterpret_cast<struct ArrowArray *>(arg0),
+                               reinterpret_cast<struct ArrowSchema *>(arg1));
+  auto table = arrow::Table::FromRecordBatches({maybeRecordBatch.ValueOrDie()});
+  return reinterpret_cast<jlong>(new (
+      (void *)rv_base) arrow::Result<std::shared_ptr<arrow::Table>>(table));
 }
 
 #ifdef __cplusplus
