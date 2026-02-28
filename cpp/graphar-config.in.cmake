2ae39d0cd01b9df2c85a768f2778ecc7f8867f2d
Shivendra Devadhe <devadheshivendra54@gmail.com>
2026-03-01 00:46:51 +0530

    feat(cpp): improve CMake configuration for portable distribution and installation

--- cpp/graphar-config.in.cmake
+++ cpp/graphar-config.in.cmake
@@ -1,31 +1,45 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
+#Licensed to the Apache Software Foundation(ASF) under one
+# or more contributor license agreements.See the NOTICE file
+#distributed with this work for additional information
+#regarding copyright ownership.The ASF licenses this file
+#to you under the Apache License, Version 2.0(the
+#"License"); you may not use this file except in compliance
+#with the License.You may obtain a copy of the License at
 #
-#   http://www.apache.org/licenses/LICENSE-2.0
+#http:  // www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+#Unless required by applicable law or agreed to in writing,
+#software distributed under the License is distributed on an
+#"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#KIND, either express or implied.See the License for the
+#specific language governing permissions and limitations
+#under the License.
 
-# - Config file for the graphar package
+#- Config file for the graphar package
 #
-# It defines the following variables
+#It defines the following variables
 #
-#  GRAPHAR_INCLUDE_DIR         - include directory for graphar
-#  GRAPHAR_INCLUDE_DIRS        - include directories for graphar
-#  GRAPHAR_LIBRARIES           - libraries to link against
+#GRAPHAR_INCLUDE_DIR - include directory for graphar
+#GRAPHAR_INCLUDE_DIRS - include directories for graphar
+#GRAPHAR_LIBRARIES - libraries to link against
 
-set(GRAPHAR_HOME "${CMAKE_CURRENT_LIST_DIR}/../../..")
-include("${CMAKE_CURRENT_LIST_DIR}/graphar-targets.cmake")
+@PACKAGE_INIT @
 
-set(GRAPHAR_LIBRARIES graphar)
-set(GRAPHAR_INCLUDE_DIR "${GRAPHAR_HOME}/include")
-set(GRAPHAR_INCLUDE_DIRS "${GRAPHAR_INCLUDE_DIR}")
+    include(CMakeFindDependencyMacro)
+
+#Only find_dependency if we didn't bundle everything
+        if (NOT @BUILD_ARROW_FROM_SOURCE
+            @) find_dependency(Arrow REQUIRED) find_dependency(Parquet REQUIRED)
+            find_dependency(ArrowDataset REQUIRED) if (@Arrow_VERSION
+                                                       @VERSION_GREATER_EQUAL
+                                                       "12.0.0")
+                find_dependency(ArrowAcero) endif() endif()
+
+                    include("${CMAKE_CURRENT_LIST_DIR}/graphar-targets.cmake")
+
+#Maintain backward compatibility for users using variables
+                        set(GRAPHAR_LIBRARIES graphar::graphar)
+                            get_target_property(
+                                GRAPHAR_INCLUDE_DIRS graphar::graphar
+                                    INTERFACE_INCLUDE_DIRECTORIES)
+                                set(GRAPHAR_INCLUDE_DIR ${GRAPHAR_INCLUDE_DIRS})
